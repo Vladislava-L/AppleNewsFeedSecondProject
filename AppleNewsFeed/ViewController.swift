ee34764366bf2001c96c6c2c8338e548887415cf
Vladislava Lutkevichus <vladislava.lutkevichus@gmail.com>
2021-02-21 11:22:35 +0200

    Initial Commit

---
+++ AppleNewsFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppleNewsFeed
+//
+//  Created by l.vladislava on 21/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
